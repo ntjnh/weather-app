c09223b5688832906455be9da7234e8b9b13d94e
Nate J.M <nate@natejonah.com>
2022-11-03 21:53:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weather App
+
+A JS app for getting weather conditions for your geographic location or a location you've searched for.
+
+The app uses the Open Weather API for weather and location data.
+
+This is an update/refresh of the [weather app I made a few years ago](https://github.com/ntjnh/local-weather) when I was learning JS through freeCodeCamp.
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { dest, parallel, series, src, watch } = require("gulp");
+const babel = require("gulp-babel");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+
+function js() {
+  return src("./src/js/*.js").pipe(babel()).pipe(dest("build/js"));
+}
+
+function scss() {
+  return src(["src/scss/app.scss", "./src/scss/projects/*.scss"])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(cleanCSS())
+    .pipe(dest("build/css"))
+    .pipe(browserSync.stream());
+}
+
+function bSync() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+    https: true,
+  });
+
+  watch("./src/js/*.js").on("change", series(js, browserSync.reload));
+  watch("./src/scss/**/*.scss", scss);
+  watch("./*.html").on("change", browserSync.reload);
+}
+
+function watchJS() {
+  watch("./src/js/**/*.js", js);
+}
+
+function watchSCSS() {
+  watch("./src/scss/**/*.scss", scss);
+}
+
+module.exports = {
+  default: series(scss, js),
+  sass: scss,
+  js: js,
+  serve: bSync,
+  watch: parallel(watchSCSS, watchJS),
+};
