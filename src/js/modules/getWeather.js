189b89894a9f7f058cb2f47514222bc07f3f54ea
Nate <codebynate@gmail.com>
2025-02-23 16:41:39 +0000

    Update API key variable reference

--- src/js/modules/getWeather.js
+++ src/js/modules/getWeather.js
@@ -1,4 +1,4 @@
-const appKey = process.env.APP_KEY;
+const appKey = import.meta.env.VITE_APP_KEY;
 import { locationHeading, conditions, loader, getFahrenheit, getIcon, toggle } from "./utilities.js";
 
 // Get current weather with coordinates
